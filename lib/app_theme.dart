82bbc179798f13a5a2ce33e92dee180e7cce3d6e
Your Name <your_email@example.com>
2026-01-18 18:19:43 +0200

    implement profile tab UI

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static const Color primaryLight = Color(0xff0E3A99);
-  static const Color darkLight = Color(0xff457AED);
+  static const Color primaryDark = Color(0xff457AED);
   static const Color backgroundLight = Color(0xffF4F7FF);
   static const Color backgroundDark = Color(0xff000F30);
   static const Color white = Color(0xffffffff);
   static const Color offWhite = Color(0xffF0F0F0);
   static const Color black = Color(0xff1C1C1C);
   static const Color grey = Color(0xff686868);
+  static const Color lightGrey = Color(0xffE9EAEB);
 
   static ThemeData lightTheme = ThemeData(
+    primaryColor: primaryLight,
     scaffoldBackgroundColor: backgroundLight,
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       backgroundColor: white,
@@ -35,6 +37,15 @@ class AppTheme {
         borderSide: BorderSide(color: offWhite),
       ),
     ),
+    listTileTheme: ListTileThemeData(
+      tileColor: white,
+      minTileHeight: 0,
+      shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(16),
+            ),
+            titleTextStyle: TextStyle(fontSize: 16, fontWeight: .w500, color: black)
+      ),
+    
     textTheme: TextTheme(
       titleLarge: TextStyle(fontSize: 20, fontWeight: .w500, color: black),
       titleMedium: TextStyle(fontSize: 16, fontWeight: .w500, color: black),
--- lib/tabs/profile/profile_tab.dart
+++ lib/tabs/profile/profile_tab.dart
@@ -1,12 +1,64 @@
+import 'package:evently_app/app_theme.dart';
+import 'package:evently_app/models/language_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfileTab extends StatelessWidget {
   const ProfileTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('profile'),
+    TextTheme textTheme = Theme.of(context).textTheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/images/route_logo.png'),
+            radius: 52,
+          ),
+          SizedBox(height: 16),
+          Text(
+            'John Safwat',
+            style: textTheme.titleLarge!.copyWith(fontWeight: .w600),
+          ),
+          Text('johnsafwat.route@gmail.com', style: textTheme.titleSmall),
+          SizedBox(height: 32),
+          SwitchListTile(
+            value: false,
+            onChanged: (value) {},
+            title: Text('Dark mode'),
+            contentPadding: EdgeInsets.symmetric(vertical: 12,horizontal: 16),
+            activeTrackColor: Theme.of(context).primaryColor,
+            inactiveTrackColor: AppTheme.lightGrey,
+            thumbColor: WidgetStatePropertyAll(AppTheme.white),
+            trackOutlineColor: WidgetStatePropertyAll(Colors.transparent),
+          ), //like usual listTile but press on all it no switch tab just
+        SizedBox(height: 16,),
+        ListTile(
+          title: Text('Language'),
+          trailing: DropdownButton(
+            value: 'en',
+            items: LanguageModel.languages.map((language)=>
+            DropdownMenuItem(value: language.code,child: Text(language.name))
+            ).toList(),
+          onChanged: (value) {
+          
+        },
+        dropdownColor: AppTheme.white,
+        borderRadius: BorderRadius.circular(16),
+        underline: SizedBox(),
+                ),
+        )
+        ,
+        SizedBox(height: 16,),
+        ListTile(title: Text('Logout',
+         ),trailing: SvgPicture.asset('assets/icons/logout.svg'),
+         contentPadding: EdgeInsets.symmetric( vertical: 12,horizontal: 16),
+         
+         )
+        ],
+      ),
     );
   }
 }
