c43db0e8eb9c26ff6e5fd6eaf202c8e0c8a37002
Your Name <your_email@example.com>
2026-01-31 18:46:58 +0200

    implement create event logic

--- lib/create_event_screen.dart
+++ lib/create_event_screen.dart
@@ -1,3 +1,4 @@
+import 'package:evently_app/firebase_service.dart';
 import 'package:evently_app/models/category_model.dart';
 import 'package:evently_app/models/event_model.dart';
 import 'package:evently_app/tabs/home/tab_item.dart';
@@ -30,168 +31,169 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(leading: ArrowBack(), title: Text('Add event')),
-      body: Column(
-        crossAxisAlignment: .start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: ClipRRect(
-              borderRadius: BorderRadiusGeometry.circular(16),
-              child: Image.asset(
-                'assets/images/${selectedCategory.imageName}.png',
-                height: MediaQuery.sizeOf(context).height * 0.23,
-
-                width: double.infinity,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: .start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(16),
+                child: Image.asset(
+                  'assets/images/${selectedCategory.imageName}.png',
+                  height: MediaQuery.sizeOf(context).height * 0.23,
+        
+                  width: double.infinity,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: DefaultTabController(
-              length: CategoryModel.categories.length,
-              child: TabBar(
-                isScrollable: true,
-                tabs: CategoryModel.categories
-                    .map(
-                      (category) => TabItem(
-                        isSelected:
-                            currentIndex ==
-                            CategoryModel.categories.indexOf(category),
-                        label: category.name,
-                        icon: category.icon,
-                      ),
-                    )
-                    .toList(),
-                dividerColor: Colors.transparent,
-                indicatorColor: Colors.transparent,
-                labelPadding: EdgeInsets.only(right: 8),
-                tabAlignment: .start,
-                padding: EdgeInsets.only(left: 16),
-                onTap: (index) {
-                  currentIndex = index;
-                  selectedCategory = CategoryModel.categories[currentIndex];
-                  setState(() {});
-                },
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: DefaultTabController(
+                length: CategoryModel.categories.length,
+                child: TabBar(
+                  isScrollable: true,
+                  tabs: CategoryModel.categories
+                      .map(
+                        (category) => TabItem(
+                          isSelected:
+                              currentIndex ==
+                              CategoryModel.categories.indexOf(category),
+                          label: category.name,
+                          icon: category.icon,
+                        ),
+                      )
+                      .toList(),
+                  dividerColor: Colors.transparent,
+                  indicatorColor: Colors.transparent,
+                  labelPadding: EdgeInsets.only(right: 8),
+                  tabAlignment: .start,
+                  padding: EdgeInsets.only(left: 16),
+                  onTap: (index) {
+                    currentIndex = index;
+                    selectedCategory = CategoryModel.categories[currentIndex];
+                    setState(() {});
+                  },
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
-            child: Form(
-              key: formKey,
-              child: Column(
-                crossAxisAlignment: .start,
-                children: [
-                  Text('Title', style: textTheme.titleMedium),
-                  SizedBox(height: 8),
-                  DefaultTextFormField(
-                    hintText: 'Event Title',
-                    controller: titleController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Title cannot be empty';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 16),
-                  Text('Description', style: textTheme.titleMedium),
-                  SizedBox(height: 8),
-                  DefaultTextFormField(
-                    maxLines: 4,
-                    hintText: 'Event Description....',
-                    controller: descriptionController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Description cannot be empty';
-                      }
-                      return null;
-                    },
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8),
-                        child: SvgPicture.asset(
-                          'assets/icons/date.svg',
-                          height: 24,
-                          width: 24,
-                          fit: .scaleDown,
+            Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: .start,
+                  children: [
+                    Text('Title', style: textTheme.titleMedium),
+                    SizedBox(height: 8),
+                    DefaultTextFormField(
+                      hintText: 'Event Title',
+                      controller: titleController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Title cannot be empty';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    Text('Description', style: textTheme.titleMedium),
+                    SizedBox(height: 8),
+                    DefaultTextFormField(
+                      maxLines: 4,
+                      hintText: 'Event Description....',
+                      controller: descriptionController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Description cannot be empty';
+                        }
+                        return null;
+                      },
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: SvgPicture.asset(
+                            'assets/icons/date.svg',
+                            height: 24,
+                            width: 24,
+                            fit: .scaleDown,
+                          ),
                         ),
-                      ),
-                      Text('Event Date', style: textTheme.titleMedium),
-                      Spacer(),
-                      TextButton(
-                        onPressed: () async {
-                          DateTime? date = await showDatePicker(
-                            context: context,
-                            firstDate: DateTime.now(),
-                            lastDate: DateTime.now().add(Duration(days: 365)),
-                            initialDate: selectedDate,
-                            initialEntryMode: .calendarOnly,
-                          );
-                          if (date != null) {
-                            selectedDate = date;
-                            setState(() {});
-                          }
-                          print(selectedDate);
-                        },
-                        child: Text(
-                          selectedDate == null
-                              ? 'Choose Date'
-                              : dateFormat.format(selectedDate!),
+                        Text('Event Date', style: textTheme.titleMedium),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () async {
+                            DateTime? date = await showDatePicker(
+                              context: context,
+                              firstDate: DateTime.now(),
+                              lastDate: DateTime.now().add(Duration(days: 365)),
+                              initialDate: selectedDate,
+                              initialEntryMode: .calendarOnly,
+                            );
+                            if (date != null) {
+                              selectedDate = date;
+                              setState(() {});
+                            }
+                            print(selectedDate);
+                          },
+                          child: Text(
+                            selectedDate == null
+                                ? 'Choose Date'
+                                : dateFormat.format(selectedDate!),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8),
-                        child: SvgPicture.asset(
-                          'assets/icons/time.svg',
-                          height: 24,
-                          width: 24,
-                          fit: .scaleDown,
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: SvgPicture.asset(
+                            'assets/icons/time.svg',
+                            height: 24,
+                            width: 24,
+                            fit: .scaleDown,
+                          ),
                         ),
-                      ),
-                      Text('Event Time', style: textTheme.titleMedium),
-                      Spacer(),
-                      TextButton(
-                        onPressed: () async {
-                          TimeOfDay? time = await showTimePicker(
-                            context: context,
-                            initialTime:
-                                selectedTime ??
-                                TimeOfDay.now(), //نفس اللي تحتها بالظبط بس مختصرة
-                            // initialTime: selectedTime != null
-                            //     ? selectedTime!
-                            //     : TimeOfDay.now(),
-                          );
-                          if (time != null) {
-                            selectedTime = time;
-                            setState(() {});
-                          }
-                        },
-                        child: Text(
-                          selectedTime?.format(context).toString() ??
-                              'Choose time',
+                        Text('Event Time', style: textTheme.titleMedium),
+                        Spacer(),
+                        TextButton(
+                          onPressed: () async {
+                            TimeOfDay? time = await showTimePicker(
+                              context: context,
+                              initialTime:
+                                  selectedTime ??
+                                  TimeOfDay.now(), //نفس اللي تحتها بالظبط بس مختصرة
+                              // initialTime: selectedTime != null
+                              //     ? selectedTime!
+                              //     : TimeOfDay.now(),
+                            );
+                            if (time != null) {
+                              selectedTime = time;
+                              setState(() {});
+                            }
+                          },
+                          child: Text(
+                            selectedTime?.format(context).toString() ??
+                                'Choose time',
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  DefaultElevatedButton(
-                    label: 'Add event',
-                    onPressed: createEvent,
-                  ),
-                ],
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    DefaultElevatedButton(
+                      label: 'Add event',
+                      onPressed: createEvent,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -200,20 +202,22 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
     if (formKey.currentState!.validate() &&
         selectedDate != null &&
         selectedTime != null) {
-          DateTime dateTime = DateTime(
-            selectedDate!.year,
-            selectedDate!.month,
-            selectedDate!.day,
-            selectedTime!.hour,
-            selectedTime!.minute
-          );
-          EventModel event = EventModel(
-            category: selectedCategory,
-            title: titleController.text,
-            description: descriptionController.text,
-            dateTime: dateTime);
-            event.toJson();
-        }
-        
+      DateTime dateTime = DateTime(
+        selectedDate!.year,
+        selectedDate!.month,
+        selectedDate!.day,
+        selectedTime!.hour,
+        selectedTime!.minute,
+      );
+      EventModel event = EventModel(
+        category: selectedCategory,
+        title: titleController.text,
+        description: descriptionController.text,
+        dateTime: dateTime,
+      );
+      FirebaseService.createEvent(event).then((_) {
+        Navigator.of(context).pop();
+      });
+    }
   }
 }
---
+++ lib/firebase_service.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:evently_app/models/event_model.dart';
+
+class FirebaseService {
+ static CollectionReference<EventModel>getEventsCollection()=>
+  FirebaseFirestore.instance
+        .collection('events')
+        .withConverter<EventModel>(
+          fromFirestore: (snapshot, _) => EventModel.fromJson(snapshot.data()!),
+          toFirestore: (event, _) => event.toJson(),);
+  static Future<void> createEvent(EventModel event) {
+   CollectionReference<EventModel>eventCollection = getEventsCollection();
+        
+        DocumentReference<EventModel> doc = eventCollection.doc();
+        event.id = doc.id;
+        return doc.set(event);
+  }
+}
