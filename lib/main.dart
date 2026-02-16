ade0a0f994648d34e7028efcaf250df3d13654c2
Your Name <your_email@example.com>
2026-02-16 23:22:08 +0200

    add shared pref to memorize the language

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,21 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   final onBoarding = prefs.getBool('onboarding') ?? false;
   await Firebase.initializeApp();
+  final settingsProvider = SettingsProvider();
+  await settingsProvider.loadLanguage();
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (_) => EventsProvider()..getEvents()),
         ChangeNotifierProvider(create: (_) => SettingsProvider()),
+        ChangeNotifierProvider.value(value: settingsProvider),
       ],
       child: EventlyApp(onBoarding: onBoarding),
     ),
--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider with ChangeNotifier {
-  ThemeMode themeMode = .system;//here is the defult(.dark,.light,.system(according to device system))  ممكن أخليها أي حاجة عادي
+  ThemeMode themeMode =
+      .system; //here is the defult(.dark,.light,.system(according to device system))  ممكن أخليها أي حاجة عادي
   bool get isDark => themeMode == .dark;
   String languageCode = 'en';
 
-  void changeTheme(ThemeMode theme){
+  Future<void> loadLanguage() async {
+    final prefs = await SharedPreferences.getInstance();
+    languageCode = prefs.getString('language') ?? 'en';
+    notifyListeners();
+  }
+
+  void changeTheme(ThemeMode theme) {
     themeMode = theme;
     notifyListeners();
   }
-void changeLanguage(String language){
-  if(languageCode==language)return;
-  languageCode = language;
-  notifyListeners();
-}
-  
+
+  Future<void> changeLanguage(String language) async {
+    if (languageCode == language) return;
+    languageCode = language;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('language', language);
+    notifyListeners();
+  }
 }
